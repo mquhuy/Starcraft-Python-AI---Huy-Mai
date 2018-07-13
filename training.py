3405b029345658882cb5d75a492ea75023551959
lthh91 <mquhuy@gmail.com>
2018-07-13 23:20:55 +0300

    Commit data
    Added some data from winning games.

--- training.py
+++ training.py
@@ -2,11 +2,19 @@ from HuyAI import HuyAI
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 import os
+import msvcrt
+import time
 
 os.environ["SC2PATH"] = 'E:/StarCraft II/'
 
-for _ in range(10):
+while True:
+    os.system('cls')
     run_game(maps.get("AbyssalReefLE"), [
         Bot(Race.Protoss, HuyAI()),
         Computer(Race.Terran, Difficulty.Hard)
     ], realtime=False)
+    print('Press Enter to quit:...')
+    time.sleep(2)
+    if msvcrt.kbhit():
+            if (msvcrt.getwch()=='\r'):
+                break
